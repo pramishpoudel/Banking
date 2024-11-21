82df28c117fbc5e41cf10568ee9198e62a3e6e49
pramishpoudel <pramishpoudel502@gmail.com>
2024-11-21 09:01:23 +0545

    change in some file

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,35 @@
-import React from 'react'
- 
- const Home = () => {
+import HeaderBox from '@components/HeaderBox'
+import TotalBalanceBox from '@components/TotalBalanceBox'
+import RightSidebar from "@components/RightSidebar"
+
+const Home = () => {
+  const loggedIn = { firstName: 'PRAMISH', lastName: 'POUDEL',email:"pramishpoudel502@gmail.com"};
    return (
-     <div>Home</div>
+     <section className="home">
+       <div className="home-content">
+         <header className="home-header">
+           < HeaderBox
+             type="greeting"
+             title="Welcome"
+             user={loggedIn?.firstName || 'Guest'}
+             subtext="Access and manage your account and transaction efficiently"
+           /> 
+
+           < TotalBalanceBox
+             accounts={[]}
+             totalBanks={1}
+             totalCurrentBalance={1250.35}
+           />
+
+         </header>
+         RECENT TRANSACTIONS
+       </div>
+       <RightSidebar 
+       user={loggedIn}
+       transaction={[]}
+       banks={[]}
+       />
+     </section>
    )
  }
  
---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { CornerUpLeft } from 'lucide-react'
+import React from 'react'
+import Countup from 'react-countup'
+
+
+const AnimatedCounter = ({amount}: {amount : number}) => {
+  return (
+    <div className="w-full">
+      <Countup
+        decimals={2}
+        decimal=","
+        prefix="$"
+        end={amount} />
+      </div>
+  )
+}
+
+export default AnimatedCounter
